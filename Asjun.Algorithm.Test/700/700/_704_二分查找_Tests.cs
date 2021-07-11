f65ebbf92b5298973f1a3d6168f09263e247dc11
Asjun <asjun2lin@outlook.com>
2021-07-11 12:10:32 +0800

    704.二分查找

---
+++ Asjun.Algorithm.Test/700/700/_704_二分查找_Tests.cs
@@ -0,0 +1,26 @@
+﻿using Asjun.Algorithm._700._700;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Asjun.Algorithm._700._700.Tests
+{
+    [TestClass()]
+    public class _704_二分查找_Tests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            var solution = new _704_二分查找();
+            var nums = new int[] { -1, 0, 3, 5, 9, 12 };
+            var target = 9;
+
+            var result = solution.Search(nums, target);
+            Assert.AreEqual(4, result);
+
+            nums = new int[] { -1, 0, 3, 5, 9, 12 };
+            target = 2;
+            result = solution.Search(nums, target);
+            Assert.AreEqual(-1, result);
+        }
+    }
+}
